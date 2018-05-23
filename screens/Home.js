5f5d2ab2da5c3a7e5fd800c21eed7c9468bd1d43
kolbjorn <kolbjorn.horgheim@gmail.com>
2018-05-24 00:22:46 +0200

    aktuelt now shows on home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,46 +1,115 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+const cheerio = require("react-native-cheerio");
+import { View, ScrollView, Text, StyleSheet, Button } from "react-native";
+import CustomHeader from "../components/CustomHeader";
 
-import { View, Text, StyleSheet, Button } from 'react-native';
-import CustomHeader from '../components/CustomHeader';
-
-import { Container, Header, Content } from 'native-base';
+import { Container, Header, Content } from "native-base";
 
 class Home extends Component {
-	/*static navigationOptions = {
+  constructor(props) {
+    super(props);
+    this.state = { aktuelt: null };
+  }
+  /*static navigationOptions = {
     drawerIcon:(
       <Image source={require('../assets/')}
     )
   }*/
 
-	async loadWebsite() {
-		const searchUrl = `https://www.rauma.kommune.no`;
-		const response = await fetch(searchUrl); // fetch page
+  async loadWebsite() {
+    console.log("loadWebsite");
+    const searchUrl = `https://www.rauma.kommune.no`;
+    const response = await fetch(searchUrl); // fetch page
 
-		const htmlString = await response.text(); // get response text
-		console.log(htmlString);
-	}
+    const htmlString = await response.text(); // get response text
+    console.log("loaded website");
+    this.loadCheerio(htmlString);
+  }
 
-	render() {
-		return (
-			<Container>
-				<CustomHeader
-					title="Hjem"
-					drawerOpen={() => this.props.navigation.navigate('DrawerOpen')}
-				/>
+  loadCheerio = async htmlString => {
+    console.log("loadCheerio");
+    const $ = await cheerio.load(htmlString);
+    let akt = [];
+    let aktuelt = [
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl01_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl02_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl03_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl04_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl05_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl06_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl07_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl08_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl09_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl10_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl11_HyperLink1"
+      ),
+      $(
+        "#ctl00_ctl00_ctl00_innhold_MidtSone_ctl01_FrontLayoutListeId_176_FrontLayoutListeId_176_sisteArtiklerListe_ctl12_HyperLink1"
+      )
+    ];
 
-				<Content
-					contentContainerStyle={{
-						flex: 1,
-						alignItems: 'center',
-						justifyContent: 'center'
-					}}
-				>
-					<Text>Home Screen</Text>
-					<Button title="hei" onPress={() => this.loadWebsite()} />
-				</Content>
-			</Container>
-		);
-	}
+    let akt1 = aktuelt[0].text();
+    let akt2 = aktuelt[1].text();
+    let akt3 = aktuelt[2].text();
+    let akt4 = aktuelt[3].text();
+    let akt5 = aktuelt[4].text();
+    let akt6 = aktuelt[5].text();
+    let akt7 = aktuelt[6].text();
+    let akt8 = aktuelt[7].text();
+    let akt9 = aktuelt[8].text();
+    let akt10 = aktuelt[9].text();
+    let akt11 = aktuelt[10].text();
+    let akt12 = aktuelt[11].text();
+    akt.push(akt1, akt2, akt3, akt4, akt5, akt6, akt7, akt8, akt9, akt10, akt11, akt12);
+    this.setState({ aktuelt: akt });
+  };
+
+  render() {
+    let content =
+      this.state.aktuelt !== null ? (
+        <Text>{this.state.aktuelt}</Text>
+      ) : (
+        <Button title="hei" onPress={() => this.loadWebsite()} />
+      );
+    console.log("oppdatert");
+    return (
+      <Container>
+        <CustomHeader title="Hjem" drawerOpen={() => this.props.navigation.navigate("DrawerOpen")} />
+
+        <Content
+          contentContainerStyle={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Text>Home Screen</Text>
+          <ScrollView>{content}</ScrollView>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 export default Home;
